4a9987574100b64fccdf67053c7bbe47325914a5
vrngupta53 <vrngupta53@gmail.com>
2020-12-26 18:09:25 +0530

    Use fork of pion/quic to fix transport close bugs

--- vendor/github.com/pion/quic/internal/wrapper/quic.go
+++ vendor/github.com/pion/quic/internal/wrapper/quic.go
@@ -7,11 +7,12 @@ import (
 	"crypto"
 	"crypto/tls"
 	"crypto/x509"
-	"fmt"
+	"errors"
 	"io"
 	"net"
+	"strings"
 
-	quic "github.com/lucas-clemente/quic-go"
+	"github.com/lucas-clemente/quic-go"
 )
 
 // Config represents the configuration of a Quic session
@@ -24,21 +25,23 @@ type Config struct {
 func getDefaultQuicConfig() *quic.Config {
 	return &quic.Config{
 		MaxIncomingStreams:                    1000,
-		MaxIncomingUniStreams:                 -1,              // disable unidirectional streams
+		MaxIncomingUniStreams:                 1000,
 		MaxReceiveStreamFlowControlWindow:     3 * (1 << 20),   // 3 MB
 		MaxReceiveConnectionFlowControlWindow: 4.5 * (1 << 20), // 4.5 MB
 		KeepAlive:                             true,
 	}
 }
 
+var errClientWithoutRemoteAddress = errors.New("quic: creating client without remote address")
+
 // Client establishes a QUIC session over an existing conn
 func Client(conn net.Conn, config *Config) (*Session, error) {
-	tlscfg := getTLSConfig(config)
 	rAddr := conn.RemoteAddr()
 	if rAddr == nil {
-		return nil, fmt.Errorf("quic: creating client without remote address")
+		return nil, errClientWithoutRemoteAddress
 	}
-	s, err := quic.Dial(newFakePacketConn(conn), rAddr, rAddr.String(), tlscfg, getDefaultQuicConfig())
+
+	s, err := quic.Dial(newFakePacketConn(conn), rAddr, rAddr.String(), getTLSConfig(config), getDefaultQuicConfig())
 	if err != nil {
 		return nil, err
 	}
@@ -47,18 +50,17 @@ func Client(conn net.Conn, config *Config) (*Session, error) {
 
 // Dial dials the address over quic
 func Dial(addr string, config *Config) (*Session, error) {
-	tlscfg := getTLSConfig(config)
-	s, err := quic.DialAddr(addr, tlscfg, getDefaultQuicConfig())
+	s, err := quic.DialAddr(addr, getTLSConfig(config), getDefaultQuicConfig())
 	if err != nil {
 		return nil, err
 	}
+
 	return &Session{s: s}, nil
 }
 
 // Server creates a listener for listens for incoming QUIC sessions
 func Server(conn net.Conn, config *Config) (*Listener, error) {
-	tlscfg := getTLSConfig(config)
-	l, err := quic.Listen(newFakePacketConn(conn), tlscfg, getDefaultQuicConfig())
+	l, err := quic.Listen(newFakePacketConn(conn), getTLSConfig(config), getDefaultQuicConfig())
 	if err != nil {
 		return nil, err
 	}
@@ -67,8 +69,7 @@ func Server(conn net.Conn, config *Config) (*Listener, error) {
 
 // Listen listens on the address over quic
 func Listen(addr string, config *Config) (*Listener, error) {
-	tlscfg := getTLSConfig(config)
-	l, err := quic.ListenAddr(addr, tlscfg, getDefaultQuicConfig())
+	l, err := quic.ListenAddr(addr, getTLSConfig(config), getDefaultQuicConfig())
 	if err != nil {
 		return nil, err
 	}
@@ -103,13 +104,37 @@ func (s *Session) OpenStream() (*Stream, error) {
 	return &Stream{s: str}, nil
 }
 
+// OpenUniStream opens and returns a new WritableStream
+func (s *Session) OpenUniStream() (*WritableStream, error) {
+	str, err := s.s.OpenUniStream()
+	if err != nil {
+		return nil, err
+	}
+	return &WritableStream{s: str}, nil
+}
+
 // AcceptStream accepts an incoming stream
 func (s *Session) AcceptStream() (*Stream, error) {
 	str, err := s.s.AcceptStream(context.TODO())
 	if err != nil {
+		if strings.HasPrefix(err.Error(), "Application error 0x0") {
+			return nil, nil // Errorcode == 0 implies session is closed without error
+		}
 		return nil, err
 	}
 	return &Stream{s: str}, nil
+}
+
+// AcceptUniStream accepts an incoming unidirectional stream and returns a ReadableStream
+func (s *Session) AcceptUniStream() (*ReadableStream, error) {
+	str, err := s.s.AcceptUniStream(context.TODO())
+	if err != nil {
+		if strings.HasPrefix(err.Error(), "Application error 0x0") {
+			return nil, nil // Errorcode == 0 implies session is closed without error
+		}
+		return nil, err
+	}
+	return &ReadableStream{s: str}, nil
 }
 
 // GetRemoteCertificates returns the certificate chain presented by remote peer.
@@ -125,7 +150,7 @@ func (s *Session) Close() error {
 // CloseWithError closes the connection with an error.
 // The error must not be nil.
 func (s *Session) CloseWithError(code uint16, err error) error {
-	var e = "nil"
+	e := "nil"
 	if err != nil {
 		e = err.Error()
 	}
